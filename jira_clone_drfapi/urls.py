6590aadb30c93daf3bcfb923d7a18c70c5d3986d
Kazuki <kazuki.hirata@sun-asterisk.com>
2022-02-15 15:27:14 +0700

    ［Django］ URLの紐付け #6

--- jira_clone_drfapi/urls.py
+++ jira_clone_drfapi/urls.py
@@ -1,5 +1,4 @@
 """jira_clone_drfapi URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
@@ -14,8 +13,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
+    path('authen/', include('djoser.urls.jwt')),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
